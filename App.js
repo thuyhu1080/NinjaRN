e4d38addf3bf2a02b1fff8433c14bfa9d5858594
anhtu0504 <anhtu5496@gmail.com>
2020-02-03 16:38:31 +0700

    add Using State

---
+++ App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  const [name, setName] = useState('anhtu');
+  const [person, setPerson] = useState({ name: 'TuBui', age: 25})
+
+  const clickHandler = () => {
+    setName('shaun');
+    setPerson({ name: 'pop3', age:44 });
+  }
+  return (
+    <View style={styles.container}>
+      <Text>My name is {name}</Text>
+      <Text>His name is {person.name} and his age is {person.age}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='update state' onPress={clickHandler} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  buttonContainer: {
+    marginTop: 20
+  }
+});
